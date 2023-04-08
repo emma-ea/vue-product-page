90024410c3939a96a37d228b6261a0a836bbb849
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-04-08 02:21:37 +0000

    list rendering

---
+++ l4/main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+         return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {'id': 2211, 'color': 'blue'},
+                {'id': 2213, 'color': 'green'},
+            ],
+            sizes: ['Small', 'Medium', 'Large'],
+        }
+    }
+});
+
+app.mount('#app');
